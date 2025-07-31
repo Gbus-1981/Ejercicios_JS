75b82fb8fc35ff094e7d44d83e468b63d54e5431
Jesus Perez <jesuspereztao@gmail.com>
2025-07-31 19:17:41 -0400

    se crea repositorio

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function convert() {
+    const amountUSD = parseFloat(document.getElementById("amount").value);
+    const tipoCambio = 745;
+    const result = document.getElementById("result");
+
+    if (!isNaN(amountUSD) && amountUSD >= 0) {
+        const amountCLP = amountUSD * tipoCambio;
+        result.textContent = `Equivale a ${amountCLP.toLocaleString("es-CL", { style: "currency", currency: "CLP", minimumFractionDigits: 0 })}.- CLP.`;
+        result.style.color = "black";
+    } else {
+        result.textContent = "Por favor, ingresa un monto válido.";
+        result.style.color = "red";
+    }
+}
